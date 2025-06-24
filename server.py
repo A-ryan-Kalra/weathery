9e840c1f0cf0ddf893c29a6fd6276bf45dfbea26
Aryan Kalra <aryan.smart832@gmail.com>
2025-06-24 23:39:51 +0530

    fixes

--- server.py
+++ server.py
@@ -18,8 +18,8 @@ def get_weather():
     city = request.args.get("city")
     weather_data = get_current_weather(city)
 
-    if not weather_data["cod"] == 200:
-        return render_template("city-not-found.html")
+    # if not weather_data["cod"] == 200:
+    #     return render_template("city-not-found.html")
 
     return render_template(
         "weather.html",
